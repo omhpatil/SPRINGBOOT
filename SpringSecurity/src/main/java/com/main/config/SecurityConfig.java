9f915e63195711d8433301055dd50aee69ce1338
Om Patil <patilom887@gmail.com>
2026-06-09 11:52:21 +0530

    in memory user management

--- SpringSecurity/src/main/java/com/main/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/main/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,12 +19,32 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public").permitAll() // anyone
-                        .requestMatchers("/admin").hasRole("ADMIN") // only admin
+                        .requestMatchers("/admin").hasRole("admin") // only admin
                         .anyRequest().authenticated() // everything else -> login
                 )
                 .formLogin(Customizer.withDefaults()) // enable login form
                 .logout(Customizer.withDefaults());    // logout functionality
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("admin")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("user")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
